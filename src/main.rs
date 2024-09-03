29591c99c0a9c58ad9e451d0721971fea9533709
nktr-cp <nktrcp@gmail.com>
2024-09-03 20:53:54 +0900

    整数1つをコンパイルするコンパイラを作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() != 2 {
+		eprintln!("引数の個数が正しくありません");
+		process::exit(1);
+	}
+
+	let number: i32 = match args[1].parse() {
+		Ok(num) => num,
+		Err(_) => {
+			eprintln!("引数には数値を指定してください");
+			process::exit(1);
+		}
+	};
+
+	println!(".intel_syntax noprefix");
+	println!(".globl main");
+	println!("main:");
+	println!("	mov rax, {}", number);
+	println!("	ret");
 }
