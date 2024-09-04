621940daeb25045feecefe650ba5c2e52ecd8a81
nktr-cp <nktrcp@gmail.com>
2024-09-04 11:14:01 +0900

    足し算と引き算を追加

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,61 @@ fn main() {
 		process::exit(1);
 	}
 
-	let number: i32 = match args[1].parse() {
-		Ok(num) => num,
-		Err(_) => {
-			eprintln!("引数には数値を指定してください");
-			process::exit(1);
-		}
-	};
+	let input = &args[1];
 
 	println!(".intel_syntax noprefix");
 	println!(".globl main");
 	println!("main:");
-	println!("	mov rax, {}", number);
-	println!("	ret");
+
+	let mut chars = input.chars().peekable();
+	let mut current_num = String::new();
+
+	while let Some(&c) = chars.peek() {
+		if c.is_digit(10) {
+			current_num.push(chars.next().unwrap());
+		} else {
+			break;
+		}
+	}
+
+	if !current_num.is_empty() {
+		println!("  mov rax, {}", current_num);
+		current_num.clear();
+	} else {
+		eprintln!("式は数字で始まる必要があります");
+		process::exit(1);
+	}
+
+	while let Some(c) = chars.next() {
+		match c {
+			'+' | '-' => {
+				let op = c;
+				while let Some(&next) = chars.peek() {
+					if next.is_digit(10) {
+						current_num.push(chars.next().unwrap());
+					} else {
+						break;
+					}
+				}
+
+				if !current_num.is_empty() {
+					match op {
+						'+' => println!("  add rax, {}", current_num),
+						'-' => println!("  sub rax, {}", current_num),
+						_ => unreachable!(),
+					}
+					current_num.clear();
+				} else {
+					eprintln!("演算子の後には数字が必要です");
+					process::exit(1);
+				}
+			},
+			_ => {
+				eprintln!("予期しない文字です: '{}'", c);
+				process::exit(1);
+			}
+		}
+	}
+
+	println!("  ret");
 }
